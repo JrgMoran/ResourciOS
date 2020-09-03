b03768b18cf6839ebd4147d69ff74f99d39e413a
Jorge Morán <jorgemoranp@gmail.com>
2020-09-03 23:05:13 +0200

    Add enums format

--- Code/ResourciOS/ResourciOS/main.swift
+++ Code/ResourciOS/ResourciOS/main.swift
@@ -11,16 +11,16 @@ import Foundation
 let defaultMode: ModeGenerateFile = .attributesClass
 
 if(CommandLine.arguments.count == 2){
-    if let opcion = ModeGenerateFile.stringToModeGenerateFile(str: CommandLine.arguments.last!) {
+    if let opcion = ModeGenerateFile(rawValue: CommandLine.arguments.last!) {
        let _ = GenerateFiles(from: FileManager.default.currentDirectoryPath, opcion: opcion)
     } else {
         let _ = GenerateFiles(from: CommandLine.arguments.last!, opcion: defaultMode)
     }
     
 } else if(CommandLine.arguments.count == 3){
-    let _ = GenerateFiles(from: CommandLine.arguments[1], opcion: ModeGenerateFile.stringToModeGenerateFile(str: CommandLine.arguments.last!) ?? defaultMode)
-} else if(CommandLine.arguments.count == 1) {
+    let _ = GenerateFiles(from: CommandLine.arguments[1], opcion: ModeGenerateFile(rawValue: CommandLine.arguments.last!) ?? defaultMode)
     
+} else if(CommandLine.arguments.count == 1) {
     let _ = GenerateFiles(from: FileManager.default.currentDirectoryPath, opcion: defaultMode)
 
 } else{
